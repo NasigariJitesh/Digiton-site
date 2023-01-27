7bef374a754c1108679190dc6072da99746cec77
NasigariJitesh <72294952+NasigariJitesh@users.noreply.github.com>
2023-01-27 19:18:21 +0530

    initial commit

--- src/components/partials/Headers/HeaderHomeFour/index.js
+++ src/components/partials/Headers/HeaderHomeFour/index.js
@@ -15,44 +15,44 @@ export default function HeaderHomeFour() {
 		<>
 			<MobileHeaderCom logo={logo2} />
 			<header className='main-header header-four'>
-				{/* <div className="header-top-wrap bg-lighter py-10">
-          <div className="container">
-            <div className="header-top">
-              <div className="top-left">
-                <ul>
-                  <li>
-                    Call Us: <a href="callto:548978478">548978478</a>
-                  </li>
-                  <li>
-                    Email us:
-                    <a href="mailto:demo@example.com">demo@example.com</a>
-                  </li>
-                  <li>Our address: 45 Dream street Austria</li>
-                </ul>
-              </div>
-              <div className="top-right">
-                <div className="office-time">
-                  <i className="far fa-clock"></i>
-                  <span>08:00 am - 06:00 pm</span>
-                </div>
-                <div className="social-style-one">
-                  <a href="http://facebook.com">
-                    <i className="fab fa-facebook-f"></i>
-                  </a>
-                  <a href="http://twitter.com">
-                    <i className="fab fa-twitter"></i>
-                  </a>
-                  <a href="https://www.instagram.com/">
-                    <i className="fab fa-instagram"></i>
-                  </a>
-                  <a href="https://www.pinterest.com/">
-                    <i className="fab fa-pinterest-p"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
+				<div className='header-top-wrap bg-lighter py-10'>
+					<div className='container'>
+						<div className='header-top'>
+							<div className='top-left'>
+								<ul>
+									<li>
+										Call Us: <a href='callto:548978478'>548978478</a>
+									</li>
+									<li>
+										Email us:
+										<a href='mailto:demo@example.com'>demo@example.com</a>
+									</li>
+									<li>Our address: 45 Dream street Austria</li>
+								</ul>
+							</div>
+							<div className='top-right'>
+								<div className='office-time'>
+									<i className='far fa-clock'></i>
+									<span>08:00 am - 06:00 pm</span>
+								</div>
+								<div className='social-style-one'>
+									<a href='http://facebook.com'>
+										<i className='fab fa-facebook-f'></i>
+									</a>
+									<a href='http://twitter.com'>
+										<i className='fab fa-twitter'></i>
+									</a>
+									<a href='https://www.instagram.com/'>
+										<i className='fab fa-instagram'></i>
+									</a>
+									<a href='https://www.pinterest.com/'>
+										<i className='fab fa-pinterest-p'></i>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 				<div className='header-upper text-white'>
 					<div className='container clearfix'>
 						<div className='header-inner d-flex align-items-center'>
