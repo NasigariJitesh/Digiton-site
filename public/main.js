aa45b019bef7e7fc3b75f3bceeb4692e19638668
NasigariJitesh <72294952+NasigariJitesh@users.noreply.github.com>
2023-02-09 17:43:54 +0530

    config change

--- public/main.js
+++ public/main.js
@@ -4,7 +4,7 @@
 const path = require('path');
 const fs = require('fs');
 
-const dirPath = path.join(__dirname, '../blogPosts');
+const dirPath = path.join(__dirname, './blogPosts');
 // const dirPathPages = path.join(__dirname, '../src/pages/content');
 const postlist = [];
 // const pagelist = [];
